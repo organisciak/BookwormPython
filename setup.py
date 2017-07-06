18c56fcfdd514052f8e89dfc38b87558f9cc7d29
Peter Organisciak <organisciak@gmail.com>
2017-07-06 08:15:38 -0600

    Version 1 setup

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from setuptools import setup, find_packages
 
-setup(name='Bwypy',
-      version='0.1',
+setup(name='bwypy',
+      version='1.0',
       description='Bwypy: Python Tools for Bookworm',
       author='Peter Organisciak',
       author_email='organisciak@gmail.com',
       url='http://bookworm.htrc.illinois.edu',
-      packages=find_packages()
+      keywords='hathitrust text-mining text-analysis bookworm',
+      license='NCSA',
+      packages=find_packages(),
+      install_requires=['pandas', "ujson"]
       )
